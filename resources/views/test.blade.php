ba512c2f72be5d624bfcd00f0cca97ced3a0bbfd
Shuchita Mahey <shuchita@live.com>
2020-11-16 15:33:48 -0800

    route wildcard

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,6 +6,7 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>{{ $name }}</h1>
+  <h1>My Blog Post</h1>
+  <p>{{ $name }}</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ Route::get('test', function () {
         'name' => request('name')
     ]);
 });
+Route::get('posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+
+    if(! array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found');
+    }
+    return view('test', [
+        'name' => $posts[$post] ?? 'nothing here yet'
+    ]);
+});
