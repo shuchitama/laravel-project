480296cb418eb8596691392b4736be1c9b3272bc
Shuchita Mahey <shuchita@live.com>
2020-11-16 15:56:11 -0800

    add new controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test', function () {
-    return view('test', [
-        'name' => request('name')
-    ]);
-});
-Route::get('posts/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
+// Route::get('posts/{post}', function ($post) {
+//     $posts = [
+//         'my-first-post' => 'Hello, this is my first blog post!',
+//         'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+//     ];
 
-    if(! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found');
-    }
-    return view('test', [
-        'name' => $posts[$post] ?? 'nothing here yet'
-    ]);
-});
+//     if(! array_key_exists($post, $posts)) {
+//         abort(404, 'Sorry, that post was not found');
+//     }
+//     return view('test', [
+//         'name' => $posts[$post] ?? 'nothing here yet'
+//     ]);
+// });
+
+Route::get('posts/{post}', [PostsController::class, 'show']);
