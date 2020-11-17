f604f91c5c73eda389a7ae3174e422e714a7ad7d
Shuchita Mahey <shuchita@live.com>
2020-11-17 14:26:34 -0800

    create Post model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
 
     {
-        $post = DB::table('posts')-> where('slug', $slug)->first();
-
-        if(! $post) {
-            abort(404);
-        }
-
         return view('test', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
