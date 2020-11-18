2757b2bd4525a5bb57e20b175f21ca37bffb747a
Shuchita Mahey <shuchita@live.com>
2020-11-17 16:54:00 -0800

    refactor

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,6 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/contact', function () {
-  return view('contact');
+Route::get('/about', function () {
+  return view('about');
 });
